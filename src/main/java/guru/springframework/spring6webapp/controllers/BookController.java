c6ec45b7814e8fc1db01d3a8dea9cd2e8513a993
Amonizz <andrea.monizza@gmail.com>
2025-03-14 18:16:51 +0100

    adding the books view fixing and annotation in controller

--- src/main/java/guru/springframework/spring6webapp/controllers/BookController.java
+++ src/main/java/guru/springframework/spring6webapp/controllers/BookController.java
@@ -1,11 +1,11 @@
 package guru.springframework.spring6webapp.controllers;
 
 import guru.springframework.spring6webapp.services.BookService;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class BookController {
 
     private final BookService bookService;
